67b6a13c603dd8209485baace7828d15c8ece185
keys8410 <yanalmeidagarcia@gmail.com>
2020-08-14 16:46:59 -0300

    Componentes

---
+++ src/Components.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Button from './components/Form/Button';
+import Form from './components/Form/Form';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <Form />
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// rafce
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
