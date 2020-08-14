e23d9e9842374796bc5ea456c4e4efe66fe83ebc
keys8410 <yanalmeidagarcia@gmail.com>
2020-08-14 16:55:23 -0300

    Componentes

--- src/Components.js
+++ src/Components.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Button from './components/Form/Button';
-import Form from './components/Form/Form';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Form from './Components/Form/Form';
 
 const App = () => {
   return (
