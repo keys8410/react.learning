945af0e6907fe65683f94681483502b76c79c099
keys8410 <yanalmeidagarcia@gmail.com>
2020-09-01 17:06:51 -0300

    CSS import

---
+++ src/AppCSS.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Css/AppCSS.css';
+import Title from './Css/Title.js';
+
+const AppCSS = () => {
+  return (
+    <div className="container">
+      <h1>Texto</h1>
+      <Title />
+    </div>
+  );
+};
+
+export default AppCSS;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './DesafioForms';
+import App from './AppCSS';
 
 ReactDOM.render(
   <React.StrictMode>
